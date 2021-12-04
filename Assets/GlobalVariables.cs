9f42b03f911e73889fa154aa13070d4d36db9b76
ollymonger <olivermonger123@gmail.com>
2021-12-04 17:33:19 +0000

    Added IP address, name and button functions to global variables

--- Assets/GlobalVariables.cs
+++ Assets/GlobalVariables.cs
@@ -1,12 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GlobalVariables : MonoBehaviour
 {
     public static GlobalVariables Instance;
 
     public bool connectToServer = false;
+
+    public string IPAddress = "";
+
+    public string selectedName = "";
+
+    public void LoadAsMultiplayer()
+    {
+        // Load Game Scene using SceneManager
+        SceneManager.LoadScene("GAME");
+        connectToServer = true;
+    }
+
+    public void LoadAsSingleplayer()
+    {   
+        // Load Game Scene using SceneManager
+        SceneManager.LoadScene("GAME");
+        connectToServer = false;
+    }
 
     void Start()
     {
