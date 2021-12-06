2c10f064e1d52b73541e63e6a1515d98865c2788
ollymonger <olivermonger123@gmail.com>
2021-12-06 01:02:55 +0000

    Lowered time between comunicating to the server/clients

--- Assets/Multiplayer.cs
+++ Assets/Multiplayer.cs
@@ -61,7 +61,7 @@ public class Multiplayer : MonoBehaviour
             };
 
             // Keep sending messages at every 0.3s
-            InvokeRepeating("SendUpdatePositionAndRotationMessage", 0.001f, 0.015f);
+            InvokeRepeating("SendUpdatePositionAndRotationMessage", 0.0005f, 0.01f);
 
             await websocket.Connect();
         }
